91b388b47c2baa57345cd80f289bb4dd39ad4ed0
catalinasy <42152137+catalinasy@users.noreply.github.com>
2018-09-19 09:36:38 -0300

    dia-19-challenge

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -37,7 +37,10 @@ function paintToCanvas(){
         return setInterval(() => {
             ctx.drawImage(video, 0, 0, width, height);
             let pixels = ctx.getImageData(0, 0, width, height);
-            pixels = redEffect(pixels);
+            //pixels = rgbSplit(pixels);
+            //ctx.globalAlpha = 0.1;
+            pixels = greenScreen(pixels);
+            ctx.putImageData(pixels, 0, 0);
         }, 16);
         
     }, 100);
@@ -46,14 +49,52 @@ function paintToCanvas(){
    
 }
 
+
 function redEffect(pixels){
-    for(let i = 0; i < pixels.length; i+=4{
+    for(let i = 0; i < pixels.data.length; i+=4) {
         pixels.data[i + 0] = pixels.data [i + 0] + 100; 
         pixels.data[i +1]= pixels.data[i+1] - 50;
         pixels.data [i+2] = pixels.data [i + 2] * 0.5 ;
-    })
+    }
+    return pixels;
 }
 
+function rgbSplit(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data [i + 0] + 100; 
+        pixels.data[i + 100]= pixels.data[i+1] - 50;
+        pixels.data [i - 550] = pixels.data [i + 2] * 0.5 ;
+    }
+    return pixels;
+}
+
+
+  function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
 
 function takePhoto(){
     snap.currentTime = 0;
